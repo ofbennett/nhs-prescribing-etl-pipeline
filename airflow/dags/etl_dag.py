10dab67c82dd8ee0b388b753e214d1b9ee5af5d4
ofbennett <ofbennett@gmail.com>
2020-03-14 09:55:36 +0000

    [airflow, refactor]: Splits the etl dag into finer grained tasks

--- airflow/dags/etl_dag.py
+++ airflow/dags/etl_dag.py
@@ -2,7 +2,6 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.postgres_operator import PostgresOperator
 from datetime import datetime, timedelta
-
 from helpers import sql_queries
 
 default_args = {
@@ -14,7 +13,7 @@ default_args = {
     'email_on_retry': False
 }
 
-dag = DAG('etl_dag',
+dag = DAG('etl_dag_split',
           default_args=default_args,
           description='Load and transform data into Postgres database with Airflow',
           schedule_interval='@monthly',
@@ -38,20 +37,53 @@ create_all_tables_if_not_exist = PostgresOperator(
     sql=sql_queries.create_all_tables
 )
 
-populate_all_staging_tables = PostgresOperator(
-    task_id="populate_all_staging_tables",
+pres_staging_table_populate = PostgresOperator(
+    task_id="pres_staging_table_populate",
     dag=dag,
     postgres_conn_id="my_postgres_conn",
-    sql=sql_queries.populate_all_staging_tables
+    sql=sql_queries.pres_staging_table_populate
 )
 
-insert_all_warehouse_tables = PostgresOperator(
-    task_id="insert_all_warehouse_tables",
+gp_prac_staging_table_populate = PostgresOperator(
+    task_id="gp_prac_staging_table_populate",
     dag=dag,
     postgres_conn_id="my_postgres_conn",
-    sql=sql_queries.insert_all_warehouse_tables
+    sql=sql_queries.gp_prac_staging_table_populate
+)
+
+bnf_info_staging_table_populate = PostgresOperator(
+    task_id="bnf_info_staging_table_populate",
+    dag=dag,
+    postgres_conn_id="my_postgres_conn",
+    sql=sql_queries.bnf_info_staging_table_populate
+)
+
+pres_fact_table_insert = PostgresOperator(
+    task_id="pres_fact_table_insert",
+    dag=dag,
+    postgres_conn_id="my_postgres_conn",
+    sql=sql_queries.pres_fact_table_insert
+)
+
+gp_pracs_dim_table_insert = PostgresOperator(
+    task_id="gp_pracs_dim_table_insert",
+    dag=dag,
+    postgres_conn_id="my_postgres_conn",
+    sql=sql_queries.gp_pracs_dim_table_insert
+)
+
+bnf_info_dim_table_insert = PostgresOperator(
+    task_id="bnf_info_dim_table_insert",
+    dag=dag,
+    postgres_conn_id="my_postgres_conn",
+    sql=sql_queries.bnf_info_dim_table_insert
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
 
-start_operator >> drop_all_tables >> create_all_tables_if_not_exist >> populate_all_staging_tables >> insert_all_warehouse_tables >> end_operator
+start_operator >> drop_all_tables >> create_all_tables_if_not_exist
+create_all_tables_if_not_exist >> [pres_staging_table_populate, gp_prac_staging_table_populate, bnf_info_staging_table_populate]
+pres_staging_table_populate >> pres_fact_table_insert
+gp_prac_staging_table_populate >> gp_pracs_dim_table_insert
+bnf_info_staging_table_populate >> bnf_info_dim_table_insert
+[pres_fact_table_insert, gp_pracs_dim_table_insert, bnf_info_dim_table_insert] >> end_operator
