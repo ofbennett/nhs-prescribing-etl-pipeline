67edf3f091c39e2f978b5de1ec83a7c8d40bc812
ofbennett <ofbennett@gmail.com>
2020-03-16 14:27:03 +0000

    [airflow, add]: Adds support for running the ETL pipeline on AWS and Redshift

---
+++ airflow/dags/etl_dag_cloud.py
@@ -0,0 +1,125 @@
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.postgres_operator import PostgresOperator
+from operators.data_quality import DataQualityOperator
+from operators.stage_redshift import StageToRedshiftOperator
+from datetime import datetime, timedelta
+from helpers import sql_queries_cloud
+
+default_args = {
+    'owner': 'ofbennett',
+    'start_date': datetime(2020, 1, 2),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'email_on_retry': False
+}
+
+dag = DAG('etl_dag_cloud',
+          default_args=default_args,
+          description='Load and transform data into AWS redshift database with Airflow',
+          schedule_interval='@monthly',
+          catchup=False,
+          max_active_runs=1
+        )
+
+start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+
+drop_all_tables = PostgresOperator(
+    task_id="drop_all_tables",
+    dag=dag,
+    postgres_conn_id="my_redshift_conn",
+    sql=sql_queries_cloud.drop_all_tables
+)
+
+create_all_tables_if_not_exist = PostgresOperator(
+    task_id="create_all_tables_if_not_exist",
+    dag=dag,
+    postgres_conn_id="my_redshift_conn",
+    sql=sql_queries_cloud.create_all_tables
+)
+
+pres_staging_table_populate = StageToRedshiftOperator(
+    task_id="pres_staging_table_populate",
+    dag=dag,
+    redshift_conn_id="my_redshift_conn",
+    aws_credentials_id="my_aws_conn",
+    table="pres_staging_table",
+    s3_bucket="prescribing-data",
+    s3_key="2019_11_Nov/T201911PDPI_BNFT.csv",
+    header=True
+)
+
+gp_prac_staging_table_populate = StageToRedshiftOperator(
+    task_id="gp_prac_staging_table_populate",
+    dag=dag,
+    redshift_conn_id="my_redshift_conn",
+    aws_credentials_id="my_aws_conn",
+    table="gp_pracs_staging_table",
+    s3_bucket="prescribing-data",
+    s3_key="2019_11_Nov/T201911ADDR_BNFT.csv",
+    header=False
+)
+
+bnf_info_staging_table_populate = StageToRedshiftOperator(
+    task_id="bnf_info_staging_table_populate",
+    dag=dag,
+    redshift_conn_id="my_redshift_conn",
+    aws_credentials_id="my_aws_conn",
+    table="bnf_info_staging_table",
+    s3_bucket="prescribing-data",
+    s3_key="BNF_Code_Information.csv",
+    header=True
+)
+
+postcode_info_staging_table_populate = StageToRedshiftOperator(
+    task_id="postcode_info_staging_table_populate",
+    dag=dag,
+    redshift_conn_id="my_redshift_conn",
+    aws_credentials_id="my_aws_conn",
+    table="postcode_info_staging_table",
+    s3_bucket="prescribing-data",
+    s3_key="postcode_info.csv",
+    header=True
+)
+
+pres_fact_table_insert = PostgresOperator(
+    task_id="pres_fact_table_insert",
+    dag=dag,
+    postgres_conn_id="my_redshift_conn",
+    sql=sql_queries_cloud.pres_fact_table_insert
+)
+
+gp_pracs_dim_table_insert = PostgresOperator(
+    task_id="gp_pracs_dim_table_insert",
+    dag=dag,
+    postgres_conn_id="my_redshift_conn",
+    sql=sql_queries_cloud.gp_pracs_dim_table_insert
+)
+
+bnf_info_dim_table_insert = PostgresOperator(
+    task_id="bnf_info_dim_table_insert",
+    dag=dag,
+    postgres_conn_id="my_redshift_conn",
+    sql=sql_queries_cloud.bnf_info_dim_table_insert
+)
+
+run_quality_checks = DataQualityOperator(
+    task_id='Run_data_quality_checks',
+    dag=dag,
+    conn_id="my_redshift_conn",
+    quality_checks=sql_queries_cloud.quality_tests
+)
+
+end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+start_operator >> drop_all_tables >> create_all_tables_if_not_exist
+create_all_tables_if_not_exist >> [pres_staging_table_populate, 
+                                    gp_prac_staging_table_populate, 
+                                    bnf_info_staging_table_populate, 
+                                    postcode_info_staging_table_populate]
+pres_staging_table_populate >> pres_fact_table_insert
+[gp_prac_staging_table_populate, postcode_info_staging_table_populate] >> gp_pracs_dim_table_insert
+bnf_info_staging_table_populate >> bnf_info_dim_table_insert
+[pres_fact_table_insert, gp_pracs_dim_table_insert, bnf_info_dim_table_insert] >> run_quality_checks
+run_quality_checks >> end_operator
---
+++ airflow/plugins/operators/stage_redshift.py
@@ -0,0 +1,48 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.aws_hook import AwsHook
+
+class StageToRedshiftOperator(BaseOperator):
+    sql_unformated = """
+        COPY {} FROM '{}'
+        ACCESS_KEY_ID '{}'
+        SECRET_ACCESS_KEY '{}'
+        region 'eu-west-2'
+        CSV
+        IGNOREHEADER 1;
+        """
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="my_redshift_conn",
+                 aws_credentials_id="my_aws_conn",
+                 table="",
+                 s3_bucket="",
+                 s3_key="",
+                 header=True,
+                 *args, **kwargs):
+
+        super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.aws_credentials_id = aws_credentials_id
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.table = table
+        self.header = header
+
+    def execute(self, context):
+        aws_hook = AwsHook(self.aws_credentials_id)
+        credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        self.log.info("Clearing data from destination Redshift table")
+        redshift.run("DELETE FROM {}".format(self.table))
+        self.log.info("Copying data from S3 to Redshift")
+        s3_path = "s3://{}/{}".format(self.s3_bucket, self.s3_key)
+        sql_formated = StageToRedshiftOperator.sql_unformated.format(self.table,
+                                                                     s3_path,
+                                                                     credentials.access_key,
+                                                                     credentials.secret_key)
+        if not self.header:
+            sql_formated = sql_formated.replace('IGNOREHEADER 1','')
+        redshift.run(sql_formated)
