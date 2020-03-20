f63c491471bda76ec43ef0f6d22b793db151d389
ofbennett <ofbennett@gmail.com>
2020-03-20 23:12:44 +0000

    [webapp, fix]: Fixes some text in web app

--- visualisation_web_app/app.py
+++ visualisation_web_app/app.py
@@ -98,7 +98,7 @@ Finally, a free and open source API called [Postcodes.io](https://postcodes.io) 
 
 # How was it made?
 
- This web app was built with **Flask**, **Plotly Dash** and **Mapbox** and is currently hosted on ... There is a very large volume of data to process to generate these visualisations so it presented an interesting engineering problem. The partially cloud based architecture I settled on is outlined in the schematic below. The ETL data pipeline was built with **Postgres**, **Apache Airflow**, **AWS Redshift**, and **S3**.
+ This web app was built with **Flask**, **Plotly Dash** and **Mapbox** and is currently hosted on DigitalOcean. There is a very large volume of data to process to generate these visualisations so it presented an interesting engineering problem. The partially cloud based architecture I settled on is outlined in the schematic below. The ETL data pipeline was built with **Postgres**, **Apache Airflow**, **AWS Redshift**, and **S3**.
  """,
  style={'color':colors['text'], 'backgroundColor':colors['background'], 'textAlign':'center', 'margin-left':80, 'margin-right':80, 'padding':10})
 
