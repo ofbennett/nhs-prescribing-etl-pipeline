5ef328be45449fbafdfb18a5b986765b8b83f681
ofbennett <ofbennett@gmail.com>
2020-03-21 09:27:29 +0000

    [airflow, refactor]: Renames file containing local sql statements

--- airflow/dags/etl_dag_local.py
+++ airflow/dags/etl_dag_local.py
@@ -3,7 +3,7 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.postgres_operator import PostgresOperator
 from operators.data_quality import DataQualityOperator
 from datetime import datetime, timedelta
-from helpers import sql_queries
+from helpers import sql_queries_local
 
 default_args = {
     'owner': 'ofbennett',
@@ -28,69 +28,69 @@ drop_all_tables = PostgresOperator(
     task_id="drop_all_tables",
     dag=dag,
     postgres_conn_id="my_postgres_conn",
-    sql=sql_queries.drop_all_tables
+    sql=sql_queries_local.drop_all_tables
 )
 
 create_all_tables_if_not_exist = PostgresOperator(
     task_id="create_all_tables_if_not_exist",
     dag=dag,
     postgres_conn_id="my_postgres_conn",
-    sql=sql_queries.create_all_tables
+    sql=sql_queries_local.create_all_tables
 )
 
 pres_staging_table_populate = PostgresOperator(
     task_id="pres_staging_table_populate",
     dag=dag,
     postgres_conn_id="my_postgres_conn",
-    sql=sql_queries.pres_staging_table_populate
+    sql=sql_queries_local.pres_staging_table_populate
 )
 
 gp_prac_staging_table_populate = PostgresOperator(
     task_id="gp_prac_staging_table_populate",
     dag=dag,
     postgres_conn_id="my_postgres_conn",
-    sql=sql_queries.gp_prac_staging_table_populate
+    sql=sql_queries_local.gp_prac_staging_table_populate
 )
 
 bnf_info_staging_table_populate = PostgresOperator(
     task_id="bnf_info_staging_table_populate",
     dag=dag,
     postgres_conn_id="my_postgres_conn",
-    sql=sql_queries.bnf_info_staging_table_populate
+    sql=sql_queries_local.bnf_info_staging_table_populate
 )
 
 postcode_info_staging_table_populate = PostgresOperator(
     task_id="postcode_info_staging_table_populate",
     dag=dag,
     postgres_conn_id="my_postgres_conn",
-    sql=sql_queries.postcode_info_staging_table_populate
+    sql=sql_queries_local.postcode_info_staging_table_populate
 )
 
 pres_fact_table_insert = PostgresOperator(
     task_id="pres_fact_table_insert",
     dag=dag,
     postgres_conn_id="my_postgres_conn",
-    sql=sql_queries.pres_fact_table_insert
+    sql=sql_queries_local.pres_fact_table_insert
 )
 
 gp_pracs_dim_table_insert = PostgresOperator(
     task_id="gp_pracs_dim_table_insert",
     dag=dag,
     postgres_conn_id="my_postgres_conn",
-    sql=sql_queries.gp_pracs_dim_table_insert
+    sql=sql_queries_local.gp_pracs_dim_table_insert
 )
 
 bnf_info_dim_table_insert = PostgresOperator(
     task_id="bnf_info_dim_table_insert",
     dag=dag,
     postgres_conn_id="my_postgres_conn",
-    sql=sql_queries.bnf_info_dim_table_insert
+    sql=sql_queries_local.bnf_info_dim_table_insert
 )
 
 run_quality_checks = DataQualityOperator(
     task_id='Run_data_quality_checks',
     dag=dag,
-    quality_checks=sql_queries.quality_tests
+    quality_checks=sql_queries_local.quality_tests
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
