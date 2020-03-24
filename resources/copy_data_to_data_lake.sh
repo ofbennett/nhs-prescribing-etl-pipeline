d637d0b591e585239d4a31bcd291615d0e64825d
ofbennett <ofbennett@gmail.com>
2020-03-24 17:14:07 +0000

    [scripts, fix]: Fixes path bug in aws s3 cp script

--- resources/copy_data_to_data_lake.sh
+++ resources/copy_data_to_data_lake.sh
@@ -3,7 +3,7 @@
 # The local paths below will need to be adapted
 # AWS credentials with suitable s3 write privileges should be in the .aws/credentials file
 
-aws s3 cp postcode_info.csv s3://prescribing-data/postcode_info.csv
+aws s3 cp postcode_info.csv s3://${1}/postcode_info.csv
 aws s3 cp 20200201_1580570906919_BNF_Code_Information.csv s3://${1}/BNF_Code_Information.csv
 aws s3 cp 2019_11_Nov/T201911PDPI\ BNFT.csv s3://${1}/2019_11_Nov/T201911PDPI_BNFT.csv
 aws s3 cp 2019_11_Nov/T201911ADDR\ BNFT.csv s3://${1}/2019_11_Nov/T201911ADDR_BNFT.csv
