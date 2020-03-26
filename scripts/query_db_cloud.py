cc0b70ac14249e02249a76519d3e0dcb34339bd5
ofbennett <ofbennett@gmail.com>
2020-03-26 14:51:53 +0000

    [scripts, refactor]: Refactors some of the cloud query script

--- scripts/query_db_cloud.py
+++ scripts/query_db_cloud.py
@@ -22,7 +22,7 @@ AND gp.longitude IS NOT NULL
 GROUP BY gp.name, gp.longitude, gp.latitude
 """
 
-q_all = """
+q_all_meds = """
 SELECT SUM(pre.nic) as total_cost, gp.name, gp.longitude, gp.latitude
 FROM pres_fact_table pre
 JOIN gp_pracs_dim_table gp
@@ -46,6 +46,35 @@ def connect_db_redshift(config):
     cur = conn.cursor()
     return conn, cur
 
+def run_query_and_save_to_s3(date,med_num,med,conn,cur):
+    if med == 'All':
+        query = q_all_meds.format(year=date[:4],month=date[5:])
+    else:
+        query = q.format(med=med,year=date[:4],month=date[5:])
+    query_final = """
+            UNLOAD('{query}')
+            TO 's3://prescribing-data/unload/{year}/{month}/{med_num}/'
+            IAM_ROLE '{iam_role}'
+            CSV
+            HEADER
+            ALLOWOVERWRITE;
+            """.format(query=query,year=date[:4],month=date[5:],med_num=i,iam_role=config['CLUSTER']['IAM_ROLE'])
+    cur.execute(query_final)
+
+def download_results_from_s3(date,med_num,med,s3):
+    directory = '../visualisation_web_app/data_cloud/{year}/{month}/{med_num}'.format(year=date[:4],month=date[5:],med_num=i)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    files = s3.list_objects(Bucket = 'prescribing-data', Prefix='unload/{year}/{month}/{med_num}'.format(year=date[:4],month=date[5:],med_num=i))
+    for j, item in enumerate(files['Contents']):
+        s3.download_file('prescribing-data',item['Key'],directory+'/'+str(j)+'.csv')
+
+def process_downloaded_files(date,med_num,med):
+    paths = '../visualisation_web_app/data_cloud/{year}/{month}/{med_num}/*.csv'.format(year=date[:4],month=date[5:],med_num=i)
+    files = glob.glob(paths)
+    df_concat = pd.concat([pd.read_csv(f) for f in files])
+    df_concat.to_csv('../visualisation_web_app/data_cloud/{year}/{month}/{med_num}datafile.csv'.format(year=date[:4],month=date[5:],med_num=i),index=False)
+
 def main():
     config = configparser.ConfigParser()
     config.read('./config.cfg')
@@ -55,20 +84,7 @@ def main():
     print("Running queries and saving to s3...")
     for date in dates:
         for i, med in enumerate(meds):
-            if med == 'All':
-                query = q_all.format(year=date[:4],month=date[5:])
-            else:
-                query = q.format(med=med,year=date[:4],month=date[5:])
-            query_final = """
-                    UNLOAD('{query}')
-                    TO 's3://prescribing-data/unload/{year}/{month}/{med_num}/'
-                    IAM_ROLE '{iam_role}'
-                    CSV
-                    HEADER
-                    ALLOWOVERWRITE;
-                    """.format(query=query,year=date[:4],month=date[5:],med_num=i,iam_role=config['CLUSTER']['IAM_ROLE'])
-            cur.execute(query_final)
-
+            run_query_and_save_to_s3(date,i,med,conn,cur)
     cur.close()
     conn.close()
 
@@ -76,20 +92,12 @@ def main():
     s3 = boto3.client('s3')
     for date in dates:
         for i, med in enumerate(meds):
-            directory = '../visualisation_web_app/data_cloud/{year}/{month}/{med_num}'.format(year=date[:4],month=date[5:],med_num=i)
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-            files = s3.list_objects(Bucket = 'prescribing-data', Prefix='unload/{year}/{month}/{med_num}'.format(year=date[:4],month=date[5:],med_num=i))
-            for j, item in enumerate(files['Contents']):
-                s3.download_file('prescribing-data',item['Key'],directory+'/'+str(j)+'.csv')
+            download_results_from_s3(date,i,med,s3)
     
     print("Processing downloaded files...")
     for date in dates:
         for i, med in enumerate(meds):
-            paths = '../visualisation_web_app/data_cloud/{year}/{month}/{med_num}/*.csv'.format(year=date[:4],month=date[5:],med_num=i)
-            files = glob.glob(paths)
-            df_concat = pd.concat([pd.read_csv(f) for f in files])
-            df_concat.to_csv('../visualisation_web_app/data_cloud/{year}/{month}/{med_num}datafile.csv'.format(year=date[:4],month=date[5:],med_num=i),index=False)
+            process_downloaded_files(date,i,med)
 
 if __name__ == "__main__":
     main()
