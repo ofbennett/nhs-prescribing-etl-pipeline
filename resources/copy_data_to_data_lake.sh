1c2af06181e983ace1625c043a0072e2aa826212
ofbennett <ofbennett@gmail.com>
2020-03-26 12:48:36 +0000

    [scripts, update]: Improves the script which copies to data lake

--- resources/copy_data_to_data_lake.sh
+++ resources/copy_data_to_data_lake.sh
@@ -5,5 +5,18 @@
 
 aws s3 cp postcode_info.csv s3://${1}/postcode_info.csv
 aws s3 cp 20200201_1580570906919_BNF_Code_Information.csv s3://${1}/BNF_Code_Information.csv
-aws s3 cp 2019_11_Nov/T201911PDPI\ BNFT.csv s3://${1}/2019_11_Nov/T201911PDPI_BNFT.csv
-aws s3 cp 2019_11_Nov/T201911ADDR\ BNFT.csv s3://${1}/2019_11_Nov/T201911ADDR_BNFT.csv
+
+for i in */; 
+do
+  year=${i:0:4}
+  month=${i:5:2}
+  command1="aws s3 cp "$i"T"$year$month"PDPI\ BNFT.csv s3://"${1}"/"$year"_"$month"/T"$year$month"PDPI_BNFT"
+  command2="aws s3 cp "$i"T"$year$month"ADDR\ BNFT.csv s3://"${1}"/"$year"_"$month"/T"$year$month"ADDR_BNFT"
+  eval $command1
+  eval $command2
+done
+echo "All done"
+
+# Commands look like this
+# aws s3 cp 2019_11_Nov/T201911PDPI\ BNFT.csv s3://${1}/2019_11_Nov/T201911PDPI_BNFT.csv
+# aws s3 cp 2019_11_Nov/T201911ADDR\ BNFT.csv s3://${1}/2019_11_Nov/T201911ADDR_BNFT.csv
