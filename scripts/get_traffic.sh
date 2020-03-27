a6b4418f200115dceb46f6ea86fd5d398fa122c4
ofbennett <ofbennett@gmail.com>
2020-03-27 15:04:32 +0000

    [scripts, add]: Adds example usage to get site traffic script

--- scripts/get_traffic.sh
+++ scripts/get_traffic.sh
@@ -1,4 +1,5 @@
 # Hacky little script to get a rough idea of the site's traffic
+# Example usage: $ bash get_traffic.sh
 num=$(docker logs viz_app | wc -l)
 echo "The total hits since the site was deployed is roughly:"
 result=$(( num / 12 ))
