3e31e7379ca12fb9e5be092a1b883578e9118d0e
root <ofbennett@gmail.com>
2020-06-24 16:39:08 +0000

    [webapp, update]: Updates file paths in cron job

--- visualisation_web_app/reboot.sh
+++ visualisation_web_app/reboot.sh
@@ -1,3 +1,3 @@
-bash /root/NHS_Prescribing_ETL_Pipeline/scripts/get_traffic.sh >> record.txt
+bash NHS_Prescribing_ETL_Pipeline/scripts/get_traffic.sh >> record.txt
 /usr/local/bin/docker-compose -f NHS_Prescribing_ETL_Pipeline/visualisation_web_app/docker-compose-prod.yml down
 /usr/local/bin/docker-compose -f NHS_Prescribing_ETL_Pipeline/visualisation_web_app/docker-compose-prod.yml up -d
