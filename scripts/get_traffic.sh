a52aee97c717fd9fa1b1eb01f998af06fa552077
root <ofbennett@gmail.com>
2020-06-24 16:50:39 +0000

    [webapp, update]: Updates output of get_traffic script

--- scripts/get_traffic.sh
+++ scripts/get_traffic.sh
@@ -1,6 +1,10 @@
 # Hacky little script to get a rough idea of the site's traffic
 # Example usage: $ bash get_traffic.sh
+echo "******************************"
+date=$(date)
+echo $date
 num=$(docker logs viz_app | wc -l)
-echo "The total hits since the site was deployed is roughly:"
+echo "The total hits since the site was last deployed is roughly:"
 result=$(( num / 12 ))
 echo $result
+echo "******************************"
