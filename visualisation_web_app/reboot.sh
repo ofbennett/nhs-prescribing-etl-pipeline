8314fde2af0adc8b20703dc21ab46bb38b0f5dff
Oscar Bennett <ofbennett@gmail.com>
2022-12-06 10:57:51 +0000

    [webapp, update]: Automates SSL cert renewal

--- visualisation_web_app/reboot.sh
+++ visualisation_web_app/reboot.sh
@@ -1,3 +1,8 @@
 bash NHS_Prescribing_ETL_Pipeline/scripts/get_traffic.sh >> record.txt
-/usr/local/bin/docker-compose -f NHS_Prescribing_ETL_Pipeline/visualisation_web_app/docker-compose-prod.yml down
-/usr/local/bin/docker-compose -f NHS_Prescribing_ETL_Pipeline/visualisation_web_app/docker-compose-prod.yml up -d
+/usr/bin/docker compose -f NHS_Prescribing_ETL_Pipeline/visualisation_web_app/docker-compose-prod.yml down
+sleep 5
+/usr/bin/docker compose -f NHS_Prescribing_ETL_Pipeline/visualisation_web_app/docker-compose-init-ssl.yml up -d --build
+sleep 20
+/usr/bin/docker compose -f NHS_Prescribing_ETL_Pipeline/visualisation_web_app/docker-compose-init-ssl.yml down
+sleep 5
+/usr/bin/docker compose -f NHS_Prescribing_ETL_Pipeline/visualisation_web_app/docker-compose-prod.yml up -d --build
